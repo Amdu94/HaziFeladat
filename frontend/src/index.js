f3122257a326f017ebb80547b7cffcfddae28eae
Amdu94 <adam.dulai@gmail.com>
2024-04-22 10:03:08 +0200

    Create some components and product page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals.js';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./Components/ErrorPage/ErrorPage.jsx";
+import Menu from "./Components/Menu/Menu.jsx";
+import ProductForm from "./Pages/Index.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Menu />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/products",
+                element: <ProductForm />,
+            },
+        ],
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
